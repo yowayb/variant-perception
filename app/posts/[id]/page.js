df38fa478841d4d23e1db1c0088683d27db3c32c
Bowie Buorn <bowie@aug28.cc>
2023-09-06 22:39:04 +0300

    init tailwind css

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -17,7 +17,7 @@ export default async function Post({ params }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className="text-3xl font-bold">{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
